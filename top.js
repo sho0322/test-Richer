178ae38c980b65bfc696bd7f37b1ca6b95e33c22
sho0322 <infinity0322s@gmail.com>
2022-08-07 21:14:37 +0900

    import

--- top.js
+++ top.js
@@ -85,29 +85,4 @@ addEventListener("load", function () {
     catch (e) {
         alert("ページを再読み込みしてください。");
     }
-    //ページ遷移(URLとfooterメニューを紐付ける)
-    var NAVIGATIONN_LIST = [];
-    var pageNavigation = document.querySelector("#footer");
-    var pageNavigationChildren = Object(pageNavigation).children;
-    for (var i = 0; i < pageNavigationChildren.length; i++) {
-        Object(NAVIGATIONN_LIST).push(pageNavigationChildren[i]);
-    }
-    var PAGE_URL = [
-        "/timeline.html",
-        "/schedule.html",
-        "/search.html",
-        "/notification.html",
-        "/contact.html",
-    ];
-    var PAGE_URL_LIST = PAGE_URL.length;
-    var _loop_1 = function (i) {
-        Object(NAVIGATIONN_LIST[i]).addEventListener("click", function () {
-            setTimeout(function () {
-                location.href = PAGE_URL[i];
-            }, 280);
-        });
-    };
-    for (var i = 0; i < PAGE_URL_LIST; i++) {
-        _loop_1(i);
-    }
 });
--- top.ts
+++ top.ts
@@ -113,27 +113,4 @@ addEventListener("load", () => {
   } catch (e) {
     alert("ページを再読み込みしてください。");
   }
-  //ページ遷移(URLとfooterメニューを紐付ける)
-  let NAVIGATIONN_LIST: never[] = [];
-  const pageNavigation = document.querySelector("#footer");
-  const pageNavigationChildren = Object(pageNavigation).children;
-  for (let i = 0; i < pageNavigationChildren.length; i++) {
-    Object(NAVIGATIONN_LIST).push(pageNavigationChildren[i]);
-  }
-
-  const PAGE_URL: readonly string[] = [
-    "/timeline.html",
-    "/schedule.html",
-    "/search.html",
-    "/notification.html",
-    "/contact.html",
-  ];
-  const PAGE_URL_LIST = PAGE_URL.length;
-  for (let i = 0; i < PAGE_URL_LIST; i++) {
-    Object(NAVIGATIONN_LIST[i]).addEventListener("click", () => {
-      setTimeout(() => {
-        location.href = PAGE_URL[i];
-      }, 280);
-    });
-  }
 });
